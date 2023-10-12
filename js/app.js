8406d696b30096cc650a94ae1b333539fe8cbf8d
geovanex24 <geovanex24@hotmail.com>
2023-10-12 14:43:33 -0600

    feat: Add footer and scroll effect to images

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const imagenes = document.querySelectorAll(".propiedad__imagen");
+
+window.addEventListener("scroll", () => {
+  let scroll = this.scrollY / -20;
+  imagenes.forEach((img) => {
+    if (scroll < -80) {
+      scroll += 80;
+    }
+    img.style.backgroundPositionY = `${scroll}px`;
+  });
+});
